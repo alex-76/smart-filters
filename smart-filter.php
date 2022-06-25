280c3dec206f1dcad6ce6a7739482e6fcd0d3b72
Alex76 <alex0676@gmail.com>
2022-06-25 22:49:47 +0300

    start dev

---
+++ smart-filter.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *
+ * Plugin Name:       Smart Filter
+ * Description:       The plugin adds attributes for external links taking into account different post types.
+ * Version:           1.0.0
+ * Author:            Alex
+ * Author URI:        Alex
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       links
+ * Domain Path:       /languages
+ *
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Password. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
+ */
+
+
+//require_once( plugin_dir_path( __FILE__ ) . 'inc/plugin_links_external.php' );
