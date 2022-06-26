0515917ea2ec2a9c8324c1669b2aa206156ecec8
Alex76 <alex0676@gmail.com>
2022-06-26 19:08:57 +0300

    dev

--- inc/admin/classes/YMC_Meta_Boxes.php
+++ inc/admin/classes/YMC_Meta_Boxes.php
@@ -19,9 +19,65 @@ class YMC_Meta_Boxes {
 
 	public function ymc_top_meta_box() { ?>
 
-		<div>Header 1</div>
-		<div>Header 2</div>
+        <header class="ymc__header">
+            <div class="logo"><img src="<?php echo YMC_SMART_FILTER_URL . '/admin/assets/images/full-logo.png'; ?>"></div>
+            <div class="manage-dash">
+                <span class="dashicons dashicons-admin-tools"></span>
+                <span class="title"><?php echo esc_html__('General Settings','ymc-smart-filter'); ?></span>
+            </div>
+        </header>
 
+        <div class="ymc__container-settings">
+            <div class="tab-sidebar">
+                <ul class="nav-tabs" id="ymcTab">
+                    <li class="nav-item">
+                        <a class="link active" id="general-tab" href="#general">
+	                        <?php echo esc_html__('General','ymc-smart-filter'); ?>
+                            <span class="info"><?php echo esc_html__('Post Type, Categories','ymc-smart-filter'); ?> </span>
+                            <span class="dashicons dashicons-admin-tools"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link" id="layouts-tab" href="#layoutstab">
+			                <?php echo esc_html__('Layouts','ymc-smart-filter'); ?>
+                            <span class="info"><?php echo esc_html__('Post Layout, Filter Layout','ymc-smart-filter'); ?> </span>
+                            <span class="dashicons dashicons-editor-table"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link" id="appearance-tab" href="#appearance">
+                            <?php echo esc_html__('Appearance','ymc-smart-filter');?>
+                            <span class="info"><?php echo esc_html__('Post Layout, Filter Layout','ymc-smart-filter'); ?></span>
+                            <span class="dashicons dashicons-visibility"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link" id="typography-tab">
+                            <?php echo esc_html__('Typography','ymc-smart-filter');?>
+                            <span class="info"><?php echo esc_html__('Title, Description Fonts','ymc-smart-filter'); ?></span>
+                            <span class="dashicons dashicons-editor-spellcheck"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link" id="advanced-tab">
+                            <?php echo esc_html__('Advanced','category-ajax-filter'); ?>
+                            <span class="info"><?php echo esc_html__('Add Extra Classes to Post','ymc-smart-filter'); ?></span>
+                            <span class="dashicons dashicons-tag"></span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link" id="shortcode-tab" href="#shortcode">
+                            <?php echo esc_html__('Shortcode','category-ajax-filter'); ?>
+                            <span class="info"><?php echo esc_html__('Get Your shortcode','ymc-smart-filter'); ?></span>
+                            <span class="dashicons dashicons-shortcode"></span>
+                        </a>
+                    </li>
+
+
+                </ul>
+            </div>
+            <div class="tab-content">Tab Content</div>
+        </div>
 	<?php }
 
 	public function ymc_side_meta_box() { ?>
