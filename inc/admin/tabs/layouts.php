9fa9b85f8bd0caf9ef923267d74e5f2573229627
alex-76 <yuriymih@gmail.com>
2022-06-30 17:42:51 +0300

    ymc025

--- inc/admin/tabs/layouts.php
+++ inc/admin/tabs/layouts.php
@@ -4,6 +4,7 @@
 </div>
 
 <div class="content">
-	9999 --- Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+	abcde --- Lorem Ipsum is simply dummy text of the printing and typesetting industry.
 	Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
 </div>
+
